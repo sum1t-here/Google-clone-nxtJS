c75185b08c103220d0484f354a700ab110c34724
sumit <mazumdarsumit3@gmail.com>
2024-08-01 13:59:07 +0530

    fix: error

--- components/PaginationButtons.jsx
+++ components/PaginationButtons.jsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
-import { Suspense } from "react";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 
 export default function PaginationButtons() {
@@ -12,33 +11,27 @@ export default function PaginationButtons() {
   const startIndex = +searchParams.get("start") || 1;
 
   return (
-    <Suspense>
-      <div className="text-blue-700 flex px-10 pb-4 justify-between sm:justify-start sm:space-x-44 sm:px-0">
-        {startIndex >= 10 && (
-          <Link
-            href={`${pathname}?searchTerm=${searchTerm}&start=${
-              startIndex - 10
-            }`}
-          >
-            <div className="flex flex-col items-center hover:underline">
-              <BsChevronLeft className="h-5" />
-              <p>Previous</p>
-            </div>
-          </Link>
-        )}
-        {startIndex <= 90 && (
-          <Link
-            href={`${pathname}?searchTerm=${searchTerm}&start=${
-              startIndex + 10
-            }`}
-          >
-            <div className="flex flex-col items-center hover:underline">
-              <BsChevronRight className="h-5" />
-              <p>Next</p>
-            </div>
-          </Link>
-        )}
-      </div>
-    </Suspense>
+    <div className="text-blue-700 flex px-10 pb-4 justify-between sm:justify-start sm:space-x-44 sm:px-0">
+      {startIndex >= 10 && (
+        <Link
+          href={`${pathname}?searchTerm=${searchTerm}&start=${startIndex - 10}`}
+        >
+          <div className="flex flex-col items-center hover:underline">
+            <BsChevronLeft className="h-5" />
+            <p>Previous</p>
+          </div>
+        </Link>
+      )}
+      {startIndex <= 90 && (
+        <Link
+          href={`${pathname}?searchTerm=${searchTerm}&start=${startIndex + 10}`}
+        >
+          <div className="flex flex-col items-center hover:underline">
+            <BsChevronRight className="h-5" />
+            <p>Next</p>
+          </div>
+        </Link>
+      )}
+    </div>
   );
 }
