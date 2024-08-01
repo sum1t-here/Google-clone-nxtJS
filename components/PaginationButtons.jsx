c19207c8881ca542ef4d69e8d4ede1e99c0e993c
sumit <mazumdarsumit3@gmail.com>
2024-08-01 13:41:30 +0530

    add suspense

--- components/PaginationButtons.jsx
+++ components/PaginationButtons.jsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 
 export default function PaginationButtons() {
@@ -11,27 +12,33 @@ export default function PaginationButtons() {
   const startIndex = +searchParams.get("start") || 1;
 
   return (
-    <div className="text-blue-700 flex px-10 pb-4 justify-between sm:justify-start sm:space-x-44 sm:px-0">
-      {startIndex >= 10 && (
-        <Link
-          href={`${pathname}?searchTerm=${searchTerm}&start=${startIndex - 10}`}
-        >
-          <div className="flex flex-col items-center hover:underline">
-            <BsChevronLeft className="h-5" />
-            <p>Previous</p>
-          </div>
-        </Link>
-      )}
-      {startIndex <= 90 && (
-        <Link
-          href={`${pathname}?searchTerm=${searchTerm}&start=${startIndex + 10}`}
-        >
-          <div className="flex flex-col items-center hover:underline">
-            <BsChevronRight className="h-5" />
-            <p>Next</p>
-          </div>
-        </Link>
-      )}
-    </div>
+    <Suspense>
+      <div className="text-blue-700 flex px-10 pb-4 justify-between sm:justify-start sm:space-x-44 sm:px-0">
+        {startIndex >= 10 && (
+          <Link
+            href={`${pathname}?searchTerm=${searchTerm}&start=${
+              startIndex - 10
+            }`}
+          >
+            <div className="flex flex-col items-center hover:underline">
+              <BsChevronLeft className="h-5" />
+              <p>Previous</p>
+            </div>
+          </Link>
+        )}
+        {startIndex <= 90 && (
+          <Link
+            href={`${pathname}?searchTerm=${searchTerm}&start=${
+              startIndex + 10
+            }`}
+          >
+            <div className="flex flex-col items-center hover:underline">
+              <BsChevronRight className="h-5" />
+              <p>Next</p>
+            </div>
+          </Link>
+        )}
+      </div>
+    </Suspense>
   );
 }
--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -2,7 +2,7 @@
 import { RxCross2 } from "react-icons/rx";
 import { BsFillMicFill } from "react-icons/bs";
 import { AiOutlineSearch } from "react-icons/ai";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 function SearchBox() {
@@ -17,22 +17,24 @@ function SearchBox() {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      onChange={(e) => setTerm(e.target.value)}
-      className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center"
-    >
-      <input type="text" className="w-full focus:outline-none" value={term} />
-      <RxCross2
-        className="text-2xl text-gray-500 cursor-pointer sm:mr-2"
-        onClick={() => setTerm("")}
-      />
-      <BsFillMicFill className="hidden sm:inline-flex text-4xl text-blue-500 bprder-l-2 border-gray-300 mr-3 pl-4" />
-      <AiOutlineSearch
-        className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer"
-        onClick={handleSubmit}
-      />
-    </form>
+    <Suspense>
+      <form
+        onSubmit={handleSubmit}
+        onChange={(e) => setTerm(e.target.value)}
+        className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center"
+      >
+        <input type="text" className="w-full focus:outline-none" value={term} />
+        <RxCross2
+          className="text-2xl text-gray-500 cursor-pointer sm:mr-2"
+          onClick={() => setTerm("")}
+        />
+        <BsFillMicFill className="hidden sm:inline-flex text-4xl text-blue-500 bprder-l-2 border-gray-300 mr-3 pl-4" />
+        <AiOutlineSearch
+          className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer"
+          onClick={handleSubmit}
+        />
+      </form>
+    </Suspense>
   );
 }
 
